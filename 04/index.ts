30dc9b8a2913733fcb3119985aaa8d315bf2c15b
Joel Miller <miller345@me.com>
2020-12-04 21:02:34 +0000

    day 4 - part 1

--- 04/index.ts
+++ 04/index.ts
@@ -1,9 +1,28 @@
 console.log("Day 4");
 
-const input = await Deno.readTextFile("./04/example.txt");
-// const input = await Deno.readTextFile("./04/input.txt");
+type Passport = { [key: string]: string };
 
-let lines = input
+const required = [
+  "byr", // (Birth Year)
+  "iyr", // (Issue Year)
+  "eyr", // (Expiration Year)
+  "hgt", // (Height)
+  "hcl", // (Hair Color)
+  "ecl", // (Eye Color)
+  "pid", // (Passport ID)
+  // (optional) "cid", // (Country ID)
+];
+
+const isValid = (passport: Passport) =>
+  required.reduce(
+    (result, key) => result && Object.keys(passport).includes(key),
+    true,
+  );
+
+// const input = await Deno.readTextFile("./04/example.txt");
+const input = await Deno.readTextFile("./04/input.txt");
+
+let lines: Passport[] = input
   .split(/^$/gm)
   .map((line) =>
     [...line.matchAll(/(?<key>\w{3}):(?<value>\S+)/gm)]
@@ -11,9 +30,9 @@ let lines = input
       .reduce((obj, { key, value }) => ({ ...obj, [key]: value }), {})
   );
 
-console.log(lines);
+const part1 = lines.map(isValid).reduce((t, b) => t + (b ? 1 : 0), 0);
 
-// console.log("Part 1:", part1); //
+console.log("Part 1:", part1); // 202
 // console.log("Part 2:", part2); //
 
 export {};
