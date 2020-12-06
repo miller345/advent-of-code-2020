b232fff6ce21b1d959f4de9a5bdf49bcb65a22cc
Joel Miller <miller345@me.com>
2020-12-06 09:49:47 +0000

    Day 6 - part 2

--- 06/index.ts
+++ 06/index.ts
@@ -3,10 +3,19 @@ console.log("Day 6");
 export const getUniqueCharCount = (str: string) => (new Set([...str])).size;
 
 const input = await Deno.readTextFile("./06/input.txt");
-console.log(
-  "Part 1:",
-  input.split(/^$/gm).map((x) => x.replace(/\n/gm, "")).map(getUniqueCharCount)
-    .reduce((t, x) => t + x, 0),
-); // 6549
+// const input = await Deno.readTextFile("./06/example.txt");
+
+const groups = input.split(/^$/gm).map((x) => x.trim().split(/\n+/gm));
+const counts = groups.map((x) => getUniqueCharCount(x.flat().join("")));
+const part1 = counts.reduce((t, x) => t + x, 0);
+console.log("Part 1:", part1); // 6549
+
+const part2 = groups.map((group) =>
+  group.reduce(
+    (result, person) => result.filter((x) => [...person].includes(x)),
+    [...group[0]],
+  )
+).reduce((t, x) => t + x.length, 0);
+console.log("Part 2:", part2); // 3466
 
 export {};
