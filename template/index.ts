fc76e291cebcbfc97a6654a915a6adeae2e3fad3
Joel Miller <miller345@me.com>
2020-12-09 20:30:15 +0000

    update template

--- template/index.ts
+++ template/index.ts
@@ -1,9 +1,18 @@
 import { AOCSolver } from "../aoc.ts";
+import { getExample, getInput } from "../aoc.ts";
+
+const parse = (input: string) => {
+  //
+};
 
 const solve: AOCSolver = (input) => {
+  const x = parse(input);
   const part1 = 0;
   const part2 = 0;
   return { part1, part2 };
 };
+
+console.log(solve(await getExample(10)));
+// console.log(solve(await getInput(10)));
 
 export default solve;
